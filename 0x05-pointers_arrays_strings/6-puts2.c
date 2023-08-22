a69155671be293ec76e47c65508c11d602ad98e0
MrFornah <abdulbaifornahwise@gmail.com>
2023-08-21 19:59:20 -0700

    time

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -5,7 +5,7 @@
  * @str: input
  * Return: print
  */
-void puts22(char *str)
+void puts2(char *str)
 {
 	int longi = 0;
 	int t = 0;
@@ -18,7 +18,7 @@ void puts22(char *str)
 		longi++;
 	}
 	t = longi - 1;
-	for (o = 0 ; o <= t ; o++)
+	for (o = 0; o <= t; o++)
 	{
 		if (o % 2 == 0)
 	{
