46492fa942b1d7c3d7effddab0440187bafb6550
MrFornah <abdulbaifornahwise@gmail.com>
2023-08-29 19:32:10 -0700

    arrays_strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -9,15 +9,16 @@ char *_strstr(char *haystack, char *needle)
 {
 	for (; *haystack != '\0'; haystack++)
 	{
-	char *l = haystack;
-	char *p = needle;
-	while (*l == *p && *p != '\0')
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != '\0')
 		{
-		l++;
-		p++;
+			l++;
+			p++;
 		}
-			if (*p == '\0')
+		if (*p == '\0')
 			return (haystack);
 	}
-        return (0);
+	return (0);
 }
